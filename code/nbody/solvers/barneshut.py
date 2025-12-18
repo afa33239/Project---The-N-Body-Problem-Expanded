ada242ebb76af24e5c10043cab82219fad3c5634
Ioan-Adrian Fanea <ec24483@qmul.ac.uk>
2025-12-18 10:44:10 +0000

    Implemented barneshut, ready for testing

--- code/nbody/solvers/barneshut.py
+++ code/nbody/solvers/barneshut.py
@@ -1,7 +1,42 @@
 from code.nbody.solvers import Solver
-from code.nbody.physics import compute_accelerations
+from code.nbody.octree import OctreeNode
+
 
 class BarnesHutSolver(Solver):
+    def __init__(self, theta=0.7):
+        self.theta = theta
+
     def accelerations(self, bodies, cfg):
-        return compute_accelerations(bodies, cfg) #uses physics module function to computer accelerations, then returns them
-    
+        N = len(bodies)
+
+        ax = [0.0] * N
+        ay = [0.0] * N
+        az = [0.0] * N
+
+        xs = [b.x for b in bodies] #build the bounding octree cube, used to caclulate center and size
+        ys = [b.y for b in bodies]
+        zs = [b.z for b in bodies]
+
+        cx = 0.5 * (min(xs) + max(xs)) #calculate the center 
+        cy = 0.5 * (min(ys) + max(ys))
+        cz = 0.5 * (min(zs) + max(zs))
+
+        size = max(  # calculate the size of the cube
+            max(xs) - min(xs),
+            max(ys) - min(ys),
+            max(zs) - min(zs),
+        )
+
+        half_size = 0.5 * size + 1e-10  # small padding to avoid zero size
+
+        root = OctreeNode((cx, cy, cz), half_size) #initial octree node
+
+        for b in bodies: 
+            root.insert(b)
+
+        for i, b in enumerate(bodies):
+            ax[i], ay[i], az[i] = root.compute_accelerations(
+                b, self.theta, cfg.softening
+            )
+
+        return ax, ay, az
